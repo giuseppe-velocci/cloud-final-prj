2e29ac10585b6a1716d0fc9f2e58c7e3c3b471b2
Diana Laurenti <diana@MacBook-Pro-di-Diana-2.local>
2019-10-06 21:29:16 +0200

    Here is where it all begins

---
+++ src/classes/config/Env.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Config;
+
+class Env {
+    protected static $basepath;
+    protected static $envFile = "config.env";
+
+    protected static $config;
+   
+
+    protected static function parseEnv(string $envPath): array {
+        if (! file_exists($envPath))
+            throw new \Error("Env file not found.");
+            
+        $arr = file($envPath);
+        $config = [];
+
+        foreach ($arr AS $c) {
+            $c = trim($c);
+            if (substr($c, 0, 1) != "#" && strlen($c) > 0) {
+                $data = explode("=", $c);
+                $config[trim($data[0])] = trim($data[1]);                
+            }
+        }
+
+        return $config;
+    }
+
+
+    public static function get(string $key): string {
+        $basepath = $basepath ?? dirname(__DIR__, 3);
+        try {
+            self::$config = self::$config ?? self::parseEnv($basepath . '/' . self::$envFile);
+        } catch (\Error $e) {
+            die($e->getTrace() . $e->getMessage());
+        }
+        
+        if (! array_key_exists($key, self::$config))
+            throw new \Exception("Key not found!");
+        
+        return filter_var(self::$config[$key], FILTER_SANITIZE_URL);
+    }
+}
